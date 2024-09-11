19adc030a160b28572d0264b8218f72a0acfeb4a
Le Thi Tra My <myle@Yeyas-Laptop.local>
2024-09-11 10:08:11 +0700

    update confidential info and set up wsgi server

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 '''
 Make sure the required packages are installed: 
@@ -29,7 +31,7 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
